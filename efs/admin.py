7c2046bc14d2693a2d55bf80206dcfd5d3d87cb2
Hitesh Khatri <hitesh@Hiteshs-MacBook-Air.local>
2019-11-06 13:13:47 -0600

    committing backend

---
+++ efs/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Manager, Customer, Investment, Stock, User
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomerDetails(admin.ModelAdmin):
+
+    list_display = ['name', 'cust_number', 'address', 'city', 'state', 'email', 'cell_phone']
+    search_fields = ['name','cust_number']
+    list_filter = ['name']
+
+
+class InvestmentDetails(admin.ModelAdmin):
+
+    list_display = ['customer', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value',
+                    'recent_date']
+
+
+class ManagerDetails(admin.ModelAdmin):
+    list_display = ['name', 'address', 'city', 'state', 'email', 'cell_phone']
+
+
+class StockDetails(admin.ModelAdmin):
+    list_display = ['customer', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date']
+
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Manager, ManagerDetails)
+admin.site.register(Customer, CustomerDetails)
+admin.site.register(Investment, InvestmentDetails)
+admin.site.register(Stock,StockDetails)
